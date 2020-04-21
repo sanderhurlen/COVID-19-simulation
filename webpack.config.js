42b53247b44abbdc357a96b6b575f6ba588d9c1a
Sander Hurlen <sander.hurlen@icloud.com>
2020-04-21 18:04:09 +0200

    UPDATE to config files

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/app.ts',
+    mode: 'development',
     module: {
         rules: [{
             test: /\.tsx?$/,
@@ -16,4 +18,17 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 8008,
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            hash: true,
+            filename: './index.html',
+            template: './src/assets/index.html',
+            title: 'COVID-19 Simulator',
+        }),
+    ],
 };
