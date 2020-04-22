03fe0cf8f83beadccefad4af21160e7ec7cb6921
Sander Hurlen <sander.hurlen@icloud.com>
2020-04-22 13:32:01 +0200

    refactored graph object to own file

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import Chart from 'chart.js';
 
 // Class files
 import Simulator from './Simulator/Simulator';
+import graph from './Chart/chart';
 
 // Sketches
 import intro from './Sketch/intro';
@@ -12,46 +13,8 @@ import intro from './Sketch/intro';
 Simulator.Start();
 
 // set up a chart
-var ctx = 'myChart';
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [
-            {
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)',
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)',
-                ],
-                borderWidth: 1,
-            },
-        ],
-    },
-    options: {
-        scales: {
-            yAxes: [
-                {
-                    ticks: {
-                        beginAtZero: true,
-                    },
-                },
-            ],
-        },
-    },
-});
+var node = 'myChart';
+var myChart = new Chart(node, graph);
+
 // Start P5 canvas
 new p5(intro);
