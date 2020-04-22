ea0ad18ce09d55f834d27030a0ef51fa323fd820
Sander Hurlen <sander.hurlen@icloud.com>
2020-04-22 13:18:08 +0200

    ADD chart.js to project and see it working in app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,57 @@
+// packing files
 import p5 from 'p5';
+import Chart from 'chart.js';
+
+// Class files
 import Simulator from './Simulator/Simulator';
 
 // Sketches
 import intro from './Sketch/intro';
 
+// main
 Simulator.Start();
+
+// set up a chart
+var ctx = 'myChart';
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [
+            {
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)',
+                ],
+                borderWidth: 1,
+            },
+        ],
+    },
+    options: {
+        scales: {
+            yAxes: [
+                {
+                    ticks: {
+                        beginAtZero: true,
+                    },
+                },
+            ],
+        },
+    },
+});
 // Start P5 canvas
 new p5(intro);
