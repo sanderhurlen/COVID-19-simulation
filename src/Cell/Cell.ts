5dad3b7de8465f901a50a8d5764b3945cef33b5c
Sander Hurlen <sander.hurlen@icloud.com>
2020-04-27 10:15:32 +0200

    ADD cell to grid

---
+++ src/Cell/Cell.ts
@@ -0,0 +1,43 @@
+import Grid from '../Grid/Grid';
+
+enum CellStates {
+    'FREE',
+    'BLOCKED',
+}
+
+enum StateType {
+    'FENCE',
+}
+
+/** A cell for the simulation grid.
+ * This cell is able to hold special grid elements, for ex obstacles or other things that is necessary
+ * @author sander hurlen
+ */
+export default class Cell {
+    private _grid: Grid;
+    private _location: Location;
+
+    private _state: CellStates;
+    private _stateType: StateType | null;
+
+    constructor(grid: Grid, location: Location, state?: StateType) {
+        this._grid = grid;
+        this._location = location;
+
+        if (state != null) {
+            this._state = CellStates.BLOCKED;
+            this._stateType = state;
+        } else {
+            this._state = CellStates.FREE;
+            this._stateType = null;
+        }
+    }
+
+    public get grid(): Grid {
+        return this._grid;
+    }
+
+    public get location(): Location {
+        return this._location;
+    }
+}
