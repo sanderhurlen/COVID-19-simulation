5b693f79b82593267a5fe07e85425ecf61aac39b
Sander Hurlen <sander.hurlen@icloud.com>
2020-04-30 20:49:04 +0200

    add support for sass compiling and building. + added bulma

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,33 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/app.ts',
     mode: 'development',
     module: {
         rules: [{
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-        }, ],
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                            // options...
+                        },
+                    },
+                ],
+            },
+        ],
     },
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
@@ -27,8 +45,11 @@ module.exports = {
         new HtmlWebPackPlugin({
             hash: true,
             filename: './index.html',
-            template: './src/assets/index.html',
+            template: './src/index.html',
             title: 'COVID-19 Simulator',
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'css/assets/app.css',
         }),
     ],
 };
